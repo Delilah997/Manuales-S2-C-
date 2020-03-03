01da7717c4594b112985fb6b5624752a236a1cc5
Delilah997 <61153474+Delilah997@users.noreply.github.com>
2020-03-02 22:36:05 -0600

    Add files via upload

---
+++ S2/8_4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int numero=55;
+	if (numero < 100){
+		printf("El numero es menor que 100");
+		if (numero > 50)
+			printf(" y mayor que 50 \n");
+	}
+	printf("Fin del programa\n");
+	system ("pause");
+	return 0;	
+}
---
+++ S2/8_7.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+	int numero=36;
+	if (numero < 50)
+		printf("El numero es menor que 50 \n");
+		else
+			printf(" y mayor que 50 \n");
+	
+	printf("Fin del programa\n");
+	system ("pause");
+	return 0;	
+}
